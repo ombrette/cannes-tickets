090c52f236e540f96df522a1f60ba2feb4e4edc4
Héma Lambert <hema.lambert@opsone.net>
2017-05-11 16:36:22 +0200

    Add Booking

--- web/app.js
+++ web/app.js
@@ -1,12 +1,18 @@
 var app = angular.module("Cannes", ["ngTouch", "mm.foundation"]);
 
 app.controller('FrontController',
-    ['$scope', '$http',
-        function($scope, $http){
+    ['$scope', '$http', '$filter',
+        function($scope, $http, $filter){
             var ctrl = this;
             ctrl.films = null;
             ctrl.seances = null;
-            ctrl.dates = [14, 15, 16, 17, 18, 19, 20];
+            ctrl.dates = ['dimanche 14', 'lundi 15', 'mardi 16', 'mercredi 17', 'jeudi 18', 'vendredi 19', 'samedi 20'];
+            ctrl.credit = 7;
+            ctrl.msg = '';
+
+            // var data = $.param({
+            //     firstName: $scope.firstName
+            // });
 
             var promListing = $http.get("data/dates.json");
             promListing.then(function(response){
@@ -17,7 +23,30 @@ app.controller('FrontController',
                 ctrl.films = response.data.films;
             });
 
-            ctrl.booked = function(){
+            ctrl.booked = function(film, date){
+                var list_films = findByIdFilm(ctrl.seances.dates[date], film);
+                var booker_films = bookById(ctrl.seances.dates[date], list_films);
+            }
 
+            function bookById(data, array) {
+                var categoryArray = data.seances;
+                for (var i = 0; i < categoryArray['length']; i++) {
+                    if (array.includes(categoryArray[i]['id'])) {
+                        categoryArray[i]['book'] = 1;
+                    }
+                }
+            }
+
+            function findByIdFilm(data, idToLookFor) {
+                var categoryArray = data.seances;
+                var list_seances = [];
+
+                for (var i = 0; i < categoryArray['length']; i++) {
+                    if (categoryArray[i]['film'] == idToLookFor) {
+                        list_seances.push(categoryArray[i]['id']);
+                    }
+                }
+
+                return list_seances;
             }
         }]);
--- web/index.php
+++ web/index.php
@@ -3,22 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Cannes Tickets</title>
-    <script src="/bower_components/angular/angular.js"></script>
-    <script src="/bower_components/angular-foundation-6/dist/angular-foundation.min.js"></script>
-    <script src="/bower_components/angular-touch/angular-touch.min.js"></script>
-    <script src="/bower_components/angular-route/angular-route.min.js"></script>
-    <script src="/bower_components/ngmap/build/scripts/ng-map.js"></script>
-    <script src="http://maps.google.com/maps/api/js?key=AIzaSyDRZRUMwKhEu0Nqru3N8pb9rximlgnup4A&libraries=geometry"></script>
-    <script src="/app.js"></script>
+    <script src="/perso/cannesTickets/web/bower_components/angular/angular.js"></script>
+    <script src="/perso/cannesTickets/web/bower_components/angular-foundation-6/dist/angular-foundation.min.js"></script>
+    <script src="/perso/cannesTickets/web/bower_components/angular-touch/angular-touch.min.js"></script>
+    <script src="/perso/cannesTickets/web/bower_components/angular-route/angular-route.min.js"></script>
+    <script src="/perso/cannesTickets/web/app.js"></script>
 
-    <link rel="stylesheet" href="/bower_components/foundation-sites/dist/css/foundation-flex.min.css" />
-    <link rel="stylesheet" href="/app.css" />
+    <link rel="stylesheet" href="/perso/cannesTickets/web/bower_components/foundation-sites/dist/css/foundation-flex.min.css" />
+    <link rel="stylesheet" href="/perso/cannesTickets/web/app.css" />
 </head>
 <body>
 
 <div class="container">
     <table ng-controller="FrontController as ctrl">
-        <tr>
+        <tr class="salles">
             <th>
                 &nbsp;
             </th>
@@ -29,24 +27,27 @@
                 Lumière
             </th>
         </tr>
-        <tr ng-repeat="jour in ctrl.seances.dates">
+        <tr ng-repeat="jour in ctrl.seances.dates" class="seances">
             <th>
-                {{ctrl.dates[jour.jour]}} Mai 2016
+                {{ctrl.dates[jour.jour]}}
             </th>
             <td ng-repeat="seance in jour.seances" ng-class="{none: seance.film < 0, available: seance.book == 0}">
+              <div ng-hide="seance.film < 0">
                 <p><strong>{{ctrl.films[seance.film].titre}}</strong></p>
                 <p>{{ctrl.films[seance.film].realisateur}}</p>
                 <p>
-                    <span>{{seance.heure}}</span>
-                    <span ng-if="seance.book == 0 && seance.film >= 0"><img src="images/picto-question.png"><a ng-click="booked()">Demander</a></span>
-                    <span ng-if="seance.book == 1 && seance.film >= 0"><img src="images/picto-demande-en-attente.png"><a>Demandée</a></span>
+                    <span class="heure">{{seance.heure}}</span>
+                    <span class="question" ng-if="seance.book == 0 && seance.film >= 0"><button ng-click="ctrl.booked(seance.film, jour.jour)">Demander</button></span>
+                    <span class="attente" ng-if="seance.book == 1 && seance.film >= 0"><a>Demandée</a></span>
                 </p>
                 <p ng-if="seance.demande == 'High'">
                     <span class="red-circle"></span>
                 </p>
+              </div>
             </td>
         </tr>
     </table>
+    <p>{{ctrl.msg}}</p>
 </div>
 
 </body>
